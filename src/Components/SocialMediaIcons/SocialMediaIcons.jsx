4da1783ed7f910444469a645ae4b319c7370e9e7
Bruno DaSilva <hi.dasilvab@gmail.com>
2025-01-04 22:17:26 -0600

    added noreferrer to _blank anchor tags

--- src/Components/SocialMediaIcons/SocialMediaIcons.jsx
+++ src/Components/SocialMediaIcons/SocialMediaIcons.jsx
@@ -6,7 +6,7 @@ const SocialMediaIcons = ({ id, name, url, icon }) => {
   return (
     <>
       <li key={id} id={id}>
-        <a href={url} target="_blank">
+        <a href={url} target="_blank" rel="noreferrer">
           <img src={icon} alt={name} />
         </a>
       </li>
